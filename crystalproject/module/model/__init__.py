f3ab6664bf8fd483375b6da82bf40ead43899f81
gwh <3217854421@qq.com>
2023-12-17 15:30:49 +0800

    添加了shperenet，还没有来得及检查bug

--- crystalproject/module/model/__init__.py
+++ crystalproject/module/model/__init__.py
@@ -2,6 +2,7 @@ from crystalproject.module.model import cgcnn
 from crystalproject.module.model import gatv2
 from crystalproject.module.model import schnet
 from crystalproject.module.model import dimenetpp
+from crystalproject.module.model import spherenet
 from crystalproject.module.model import gcn
 from crystalproject.module.model import toponet
 
